e29aead99b6acff9cf970f1450bd184c25ea62e6
Camille <xdghost.kamil@gmail.com>
2023-04-14 21:38:47 +0300

    Added first draft of the word information screen

--- lib/word_search/presentation/pages/word_information_page.dart
+++ lib/word_search/presentation/pages/word_information_page.dart
@@ -16,7 +16,9 @@ class WordInformationPage extends StatelessWidget {
             icon: const Icon(Icons.arrow_back),
             onPressed: () {},
           ),
-          title: const Text("word"),
+          title: const Text(
+            "word",
+          ),
           actions: [
             Padding(
               padding: const EdgeInsets.only(right: 16.0),
@@ -28,12 +30,31 @@ class WordInformationPage extends StatelessWidget {
           ],
         ),
         SliverToBoxAdapter(
-          child: Card(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children:
-                  List.generate(10, (index) => const WordInformationSection()),
-            ),
+          child: Column(
+            children: [
+              Container(
+                // This container represents the image showing the queried word
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.width,
+                decoration: const BoxDecoration(color: Colors.orangeAccent),
+              ),
+              Card(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Text(
+                            "Full word description",
+                            style: Theme.of(context).textTheme.bodyLarge,
+                          ),
+                        )
+                      ] +
+                      List<Widget>.generate(
+                          10, (index) => const WordInformationSection()),
+                ),
+              ),
+            ],
           ),
         )
       ],
