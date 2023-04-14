a6ffb2afebf8db583299fd4541d28b2bacce7000
Camille <xdghost.kamil@gmail.com>
2023-04-14 13:29:30 +0300

    added key to the constructor

--- lib/word_search/presentation/pages/test_page.dart
+++ lib/word_search/presentation/pages/test_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TestPage extends StatelessWidget {
+  const TestPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
