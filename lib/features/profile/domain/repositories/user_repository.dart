0d6861cf288ef3099c68fdc4cce1ecd2b7a08fbc
ProskurinaSA <sonp2401@gmail.com>
2023-04-14 14:15:15 +0300

    refactor UserRepository

--- lib/features/profile/domain/repositories/user_repository.dart
+++ lib/features/profile/domain/repositories/user_repository.dart
@@ -3,5 +3,4 @@ import 'package:word_couch/features/profile/domain/entities/user_entity.dart';
 
 abstract class UserRepository {
   Future<Either<String, UserEntity>> signIn(String email, String password);
-
 }
