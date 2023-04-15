b298652c249bac9b918525f5b7159b23da389ee5
ProskurinaSA <sonp2401@gmail.com>
2023-04-15 14:38:39 +0300

    add constants

---
+++ lib/core/constants/firebase_const.dart
@@ -0,0 +1,9 @@
+class FirebaseConst {
+  static const pathUser = "user";
+  static const story = "story";
+  static const favourite = "favourite";
+  static const id = "id";
+  static const email = "email";
+  static const uid = "id";
+  static const test = "test";
+}
