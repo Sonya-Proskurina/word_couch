c8e6d1b1af74c396d1a408768d7d0340292e8f90
ProskurinaSA <sonp2401@gmail.com>
2023-04-15 14:35:57 +0300

    update MainPage

--- lib/features/word_search/presentation/pages/main_page.dart
+++ lib/features/word_search/presentation/pages/main_page.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:word_couch/core/di.dart';
+import 'package:word_couch/features/profile/presentation/manager/user/user_manager.dart';
+import 'package:word_couch/features/profile/presentation/pages/profile_drawer.dart';
 import '../widgets/main_screen_list.dart';
 import '../widgets/search_bar.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends ConsumerStatefulWidget {
   const MainPage({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends ConsumerState<MainPage> {
+  late GlobalKey<ScaffoldState> _scaffoldKey;
+
+  @override
+  void initState() {
+    super.initState();
+    ProfileManager manager = ref.read(DI.profileManager);
+    WidgetsBinding.instance.addPostFrameCallback((_) => manager.loading());
+
+    _scaffoldKey = GlobalKey<ScaffoldState>();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
+      drawer: const Drawer(
+        child: ProfileDrawer(),
+      ),
       resizeToAvoidBottomInset: true,
       body: Material(
         child: Stack(
@@ -17,7 +41,10 @@ class MainPage extends StatelessWidget {
               slivers: <Widget>[
                 SliverAppBar.large(
                   leading: IconButton(
-                      icon: const Icon(Icons.menu), onPressed: () {}),
+                      icon: const Icon(Icons.menu),
+                      onPressed: () {
+                        _scaffoldKey.currentState!.openDrawer();
+                      }),
                   title: Text(AppLocalizations.of(context)!.testText),
                 ),
                 const SliverToBoxAdapter(
