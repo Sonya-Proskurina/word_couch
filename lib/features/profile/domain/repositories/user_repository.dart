bcec3129056a7c5fc071f0a50de52dc703c0a266
ProskurinaSA <sonp2401@gmail.com>
2023-04-16 22:09:46 +0300

    refactor UserRepository

--- lib/features/profile/domain/repositories/user_repository.dart
+++ lib/features/profile/domain/repositories/user_repository.dart
@@ -3,6 +3,10 @@ import 'package:word_couch/features/profile/domain/entities/user_entity.dart';
 
 abstract class UserRepository {
   Future<Either<String, UserEntity>> signIn(String email, String password);
-  Future<Either<String, UserEntity>> registration(String email, String password);
+
+  Future<Either<String, UserEntity>> registration(String email);
+
   Future<Either<String, UserEntity?>> getUser();
+
+  Future<Either<String, bool>> exit();
 }
