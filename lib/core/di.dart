7920593156f364eb93db8aea289e937ceddcad4b
ProskurinaSA <sonp2401@gmail.com>
2023-04-16 22:04:30 +0300

    update di

--- lib/core/di.dart
+++ lib/core/di.dart
@@ -1,15 +1,9 @@
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:riverpod/riverpod.dart';
 import 'package:word_couch/features/profile/data/data_sources/user_auth_data_source.dart';
-import 'package:word_couch/features/profile/data/data_sources/user_local_data_source.dart';
-import 'package:word_couch/features/profile/data/data_sources/user_registration_data_source.dart';
 import 'package:word_couch/features/profile/data/repositories/user_repository_impl.dart';
 import 'package:word_couch/features/profile/presentation/manager/auth/auth_manager.dart';
 import 'package:word_couch/features/profile/presentation/manager/auth/auth_notifier.dart';
 import 'package:word_couch/features/profile/presentation/manager/auth/auth_states.dart';
-import 'package:word_couch/features/profile/presentation/manager/registration/registration_manager.dart';
-import 'package:word_couch/features/profile/presentation/manager/registration/registration_notifier.dart';
-import 'package:word_couch/features/profile/presentation/manager/registration/registration_states.dart';
 import 'package:word_couch/features/profile/presentation/manager/user/user_manager.dart';
 import 'package:word_couch/features/profile/presentation/manager/user/user_notifier.dart';
 import 'package:word_couch/features/profile/presentation/manager/user/user_states.dart';
@@ -24,24 +18,9 @@ class DI {
 
   static final userAuthDataSource = Provider((ref) => UserAuthDataSource());
 
-  static final userRegistrationDataSource =
-      Provider((ref) => UserRegistrationDataSource());
-
-  static final androidOptions = Provider((ref) => const AndroidOptions(
-        encryptedSharedPreferences: true,
-      ));
-
-  static final secureStorage = Provider(
-      (ref) => FlutterSecureStorage(aOptions: ref.watch(androidOptions)));
-
-  static final userLocalDataSource =
-      Provider((ref) => UserLocalDataSource(storage: ref.watch(secureStorage)));
-
   static final userRepository = Provider(
     (ref) => UserRepositoryImpl(
       userAuthDataSource: ref.watch(userAuthDataSource),
-      userLocalDataSource: ref.watch(userLocalDataSource),
-      userRegistrationDataSource: ref.watch(userRegistrationDataSource),
     ),
   );
 
@@ -58,15 +37,6 @@ class DI {
       StateNotifierProvider<AuthNotifier, AuthState>((ref) {
     return AuthNotifier();
   });
-
-  static final registrationNotifier =
-      StateNotifierProvider<RegistrationNotifier, RegistrationState>((ref) {
-    return RegistrationNotifier();
-  });
-
-  static final registrationManager = Provider((ref) => RegistrationManager(
-      notifier: ref.watch(registrationNotifier.notifier),
-      userRepository: ref.watch(userRepository)));
 
   static final authManager = Provider((ref) => AuthManager(
       notifier: ref.watch(authNotifier.notifier),
