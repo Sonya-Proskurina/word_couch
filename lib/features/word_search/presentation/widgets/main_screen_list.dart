685c4fcdd36c68baec5836a7ef49c8e8567a8123
ProskurinaSA <sonp2401@gmail.com>
2023-04-17 15:46:13 +0300

    update MainScreenList

--- lib/features/word_search/presentation/widgets/main_screen_list.dart
+++ lib/features/word_search/presentation/widgets/main_screen_list.dart
@@ -2,12 +2,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:word_couch/core/di.dart';
+import 'package:word_couch/features/profile/domain/entities/user_word_entity.dart';
+import 'package:word_couch/features/word_search/presentation/widgets/history_item_widget.dart';
 import 'package:word_couch/features/word_search/presentation/widgets/start_challenge_card.dart';
 
 import '../../../../core/navigation/router_path.dart';
 
 class MainScreenList extends ConsumerWidget {
-  final List<String> list;
+  final List<UserWordEntity> list;
 
   const MainScreenList({
     super.key,
@@ -34,21 +36,19 @@ class MainScreenList extends ConsumerWidget {
     return Column(
       children: <Widget>[const StartChallengeCard()] +
           ListTile.divideTiles(
-              context: context,
-              tiles: List.generate(
-                  list.length,
-                  (index) => ListTile(
-                        onTap: () {
-                          arg.setState("car");
-                          Navigator.pushNamed(
-                              context, RouterPathContainer.wordInformationPage);
-                        },
-                        title: Text(
-                          list[index],
-                          style: Theme.of(context).textTheme.titleLarge,
-                        ),
-                        subtitle: Text(list[index]),
-                      ))).toList(),
+            context: context,
+            tiles: List.generate(
+              list.length,
+              (index) => HistoryItemWidget(
+                userWordEntity: list[index],
+                tap: () {
+                  arg.setState(list[index].word);
+                  Navigator.pushNamed(
+                      context, RouterPathContainer.wordInformationPage);
+                },
+              ),
+            ),
+          ).toList(),
     );
   }
 }
