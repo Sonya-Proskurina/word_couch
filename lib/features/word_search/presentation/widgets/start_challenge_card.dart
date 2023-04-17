a476fdfc1c20f6703483535520cb3d2b9ca20f9d
Camille <xdghost.kamil@gmail.com>
2023-04-17 15:53:00 +0300

    removed unused comment

--- lib/features/word_search/presentation/widgets/start_challenge_card.dart
+++ lib/features/word_search/presentation/widgets/start_challenge_card.dart
@@ -46,36 +46,3 @@ class StartChallengeCard extends StatelessWidget {
     );
   }
 }
-
-/*Container(
-        decoration: BoxDecoration(
-            color: Theme.of(context).colorScheme.primaryContainer,
-            borderRadius: BorderRadius.circular(6.0)),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: InkWell(
-            onTap: () {
-              Navigator.pushNamed(
-                context,
-                RouterPathContainer.challengePage,
-              );
-            },
-            child: Row(
-              children: [
-                Expanded(
-                    child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Test your knowledge!",
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                    const Text("Play a game where you guess words meaning")
-                  ],
-                )),
-                const Icon(Icons.keyboard_arrow_right_outlined)
-              ],
-            ),
-          ),
-        ),
-      )*/
