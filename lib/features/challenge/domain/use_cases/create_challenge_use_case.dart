7e3b561f23d44bd23323e13a2c58fc7747a7f3ff
Camille <xdghost.kamil@gmail.com>
2023-04-18 15:52:33 +0300

    appended functionality to the basic functionality of the challenge

--- lib/features/challenge/domain/use_cases/create_challenge_use_case.dart
+++ lib/features/challenge/domain/use_cases/create_challenge_use_case.dart
@@ -1,20 +1,27 @@
 import 'dart:math';
 
 import 'package:dartz/dartz.dart';
+import 'package:word_couch/features/challenge/domain/entities/binary_answer_entity.dart';
 import 'package:word_couch/features/challenge/domain/use_cases/question_type_generator.dart';
 
+import '../../../../core/logger.dart';
+import '../../../word_information/data/models/word_model.dart';
+import '../../../word_information/domain/repositories/word_info_repository.dart';
 import '../entities/question_entity.dart';
 import '../entities/empty_question_entity.dart';
 
 class CreateChallengeUseCase {
+  final WordInfoRepository rep;
   final int synonymsAmount;
   final int antonymsAmount;
+  final Random random = Random();
 
   CreateChallengeUseCase(
-      {required this.synonymsAmount, required this.antonymsAmount});
+      {required this.synonymsAmount,
+      required this.antonymsAmount,
+      required this.rep});
 
   Iterable<QuestionType> _buildQuestionType() sync* {
-    final random = Random();
     final generators = [
       QuestionTypeGenerator(
           amount: synonymsAmount, type: QuestionType.findSynonym),
@@ -37,14 +44,90 @@ class CreateChallengeUseCase {
     yield QuestionType.none;
   }
 
-  Iterable<Either<QuestionEntity, EmptyQuestionEntity>> buildQuestion() sync* {
-    final questions = synonymsAmount + antonymsAmount;
-    for (final _ in Iterable.generate(questions)) {
-      final type = _buildQuestionType();
-      // Get a random word;
-      // Take its random synonym + one similar word
-      // yield the result
+  QuestionEntity? buildAntonymQuestion(WordModel model, String word) {
+    final antonyms = model.results[0].antonyms;
+    final similar = model.results[0].similarTo;
+    if (antonyms != null &&
+        antonyms.isNotEmpty &&
+        similar != null &&
+        similar.isNotEmpty) {
+      final antonymWord = antonyms[random.nextInt(antonyms.length)];
+      final similarWord = similar[random.nextInt(similar.length)];
+      return QuestionEntity(
+          question: 'What is antonymous to the $word?',
+          answers: [
+            BinaryAnswerEntity(answer: antonymWord, isCorrect: true),
+            BinaryAnswerEntity(answer: similarWord, isCorrect: false)
+          ],
+          type: QuestionType.findAntonym);
     }
-    yield Right(EmptyQuestionEntity());
+    return null;
+  }
+
+  QuestionEntity? buildSynonymQuestion(WordModel model, String word) {
+    final synonyms = model.results[0].synonyms;
+    final similar = model.results[0].similarTo;
+    if (synonyms != null &&
+        synonyms.isNotEmpty &&
+        similar != null &&
+        similar.isNotEmpty) {
+      final randomSynonymWord = synonyms[random.nextInt(synonyms.length)];
+      // Find words that are similar to the original one, but not synonyms
+      final similarButNotSynonym =
+          similar.toSet().difference(synonyms.toSet()).toList();
+      if (similarButNotSynonym.isNotEmpty) {
+        final randomSimilarWord =
+            similarButNotSynonym[random.nextInt(similarButNotSynonym.length)];
+        return QuestionEntity(
+          question: 'What is synonymous to the $word?',
+          answers: [
+            BinaryAnswerEntity(answer: randomSynonymWord, isCorrect: true),
+            BinaryAnswerEntity(answer: randomSimilarWord, isCorrect: false)
+          ],
+          type: QuestionType.findSynonym,
+        );
+      }
+    }
+    return null;
+  }
+
+  Future<QuestionEntity?> buildQuestion() async {
+    final type = _buildQuestionType().first;
+    if (type == QuestionType.none) {
+      return null;
+    }
+    QuestionEntity? question;
+    int i = 0;
+    // Try to generate a question of the specified QuestionType.
+    // It may happen there is not enough similar or synonym words for a random
+    // word, so it will try to generate one 10 times, and give up if it's unlucky.
+    while (question == null && i < 10) {
+      final word = await rep.getRandomWord();
+      word.fold((l) {
+        logger.e(l);
+        // Something wrong with the internet, no need to try again, return null
+        return null;
+      }, (r) {
+        switch (type) {
+          case QuestionType.findSynonym:
+            {
+              // TODO add word
+              question = buildSynonymQuestion(r);
+              break;
+            }
+          case QuestionType.findAntonym:
+            {
+              // TODO add word
+              question = buildAntonymQuestion(r);
+              break;
+            }
+          // We already know it's not none
+          case QuestionType.none:
+            break;
+        }
+      });
+      i++;
+    }
+    return question;
   }
 }
