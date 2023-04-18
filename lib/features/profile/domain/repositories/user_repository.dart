a6d49d16c72f924b0ff2e17635f7234dde118806
ProskurinaSA <sonp2401@gmail.com>
2023-04-19 02:04:33 +0300

    update profile domain

--- lib/features/profile/domain/repositories/user_repository.dart
+++ lib/features/profile/domain/repositories/user_repository.dart
@@ -1,21 +1,14 @@
 import 'package:dartz/dartz.dart';
 import 'package:word_couch/features/profile/domain/entities/user_entity.dart';
-import 'package:word_couch/features/profile/domain/entities/user_word_entity.dart';
 
 abstract class UserRepository {
-  Future<Either<String, UserEntity>> signIn(String email, String password);
-
   Future<Either<String, UserEntity>> registration(String email);
 
   Future<Either<String, UserEntity?>> getUser();
 
-  Future<Either<String, List<UserWordEntity>>> getWordsHistory();
-
-  Future<Either<String, List<UserWordEntity>>> getWordsFavorite();
-
   Future<Either<String, bool>> exit();
 
-  Future<Either<String, bool>> addFavorite(String word);
+  Future<Either<String, bool>> addFavorite(String word, bool add);
 
-  Future<Either<String, bool>> addHistory(String word);
+  Future<Either<String, bool>> addHistory(String word, String description);
 }
